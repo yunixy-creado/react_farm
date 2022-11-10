1386fbde4247d6291f9c9d7bcc568cd9577618b6
naopandao <nao_yoshida@yunixy.co.jp>
2022-11-10 16:10:37 +0900

    Redux実装するもcsrftoken取得のaxiosは確立できていない。App.tsxを修正する必要あり。

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+// module.exports ={
+  // webpack: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src/'),
+  //   },
+  // },
+  // style: {
+  //   postcss: {
+  //     plugins: [require('tailwindcss'), require('autoprefixer')],
+  //   },
+  // },
+// }
